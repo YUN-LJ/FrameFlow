778cc8154c39ca81b0a56d7c10c1419da0be0f4b
YUN-LJ <3028405217@qq.com>
2025-09-30 16:10:34 +0800

    Fun库修改

--- Fun/Norm/image.py
+++ Fun/Norm/image.py
@@ -1,7 +1,70 @@
 """提供简易的照片处理"""
 from PIL import Image, ImageFile
 
-from . import file
+from . import file, get
+
+
+def SetWallpaperReg(image_path: str) -> bool:
+    """
+    用于将照片设置为壁纸
+
+    :param image_path:照片路径,不传入时需要使用save()
+    设置成功返回True
+    """
+    if not file.check_image(image_path):
+        raise TypeError(f'文件不是图像格式{image_path}')
+    import win32gui, win32con, win32api
+    # 打开指定注册表路径
+    reg_key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
+    # 最后的参数:2拉伸,0居中,6适应,10填充,0平铺
+    win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "10")
+    # 最后的参数:1表示平铺,拉伸居中等都是0
+    win32api.RegSetValueEx(reg_key, "TileWallpaper", 0, win32con.REG_SZ, "0")
+    # 刷新桌面与设置壁纸
+    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, image_path, win32con.SPIF_SENDWININICHANGE)
+    return True
+
+
+def SetWallpaperAPI(image_data) -> bool:
+    """
+    从内存中的图像数据设置Windows壁纸
+
+    :param image_data:内存中的图像数据，可以是PIL Image对象或字节流
+    """
+    import ctypes, os, tempfile, io
+    # 如果是PIL Image对象，转换为字节流
+    if isinstance(image_data, Image.Image):
+        img_byte_arr = io.BytesIO()
+        # 保存为BMP格式，Windows壁纸最好使用BMP格式
+        image_data.save(img_byte_arr, format='BMP')  # 保存为二进制数据流如同保存在物理磁盘上一样
+        img_byte_arr = img_byte_arr.getvalue()  # 获取二进制数据,用于网络传输、内存操作
+    else:
+        img_byte_arr = image_data
+
+    # 创建一个临时文件，关闭后自动删除
+    with tempfile.NamedTemporaryFile(suffix='.bmp', delete=False) as temp_file:
+        temp_file.write(img_byte_arr)
+        temp_path = temp_file.name
+
+    try:
+        # 设置壁纸
+        SPI_SETDESKWALLPAPER = 20  # 系统操作符
+        # 调用Windows API设置壁纸
+        ctypes.windll.user32.SystemParametersInfoW(
+            SPI_SETDESKWALLPAPER,  # 操作标识符,20表示的操作为更改壁纸
+            0,  # 动态参数,更改壁纸不需要用到此参数
+            temp_path,  # 壁纸路径
+            3  # SPIF_UPDATEINIFILE | SPIF_SENDCHANGE 更新方式
+            # 如果只使用SPIF_UPDATEINIFILE(值为1)壁纸设置会保存但可能不会立即刷新;
+            # 如果只使用 SPIF_SENDCHANGE(值为2)壁纸会立即刷新但可能不会保存到系统配置中。
+        )
+    finally:
+        # 无论是否成功，都删除临时文件
+        try:
+            os.remove(temp_path)
+        except:
+            pass
+    return True
 
 
 class Image_PIL:
@@ -76,9 +139,9 @@ class Image_PIL:
         self.__image = self.__image.resize((width, height), Image.Resampling.LANCZOS)
         return width, height
 
-    def Zip(self, max_size=15, quality=100) -> ImageFile.ImageFile:
+    def zip(self, max_size=15, quality=100) -> ImageFile.ImageFile:
         """
-        将照片压缩到小于限制(图像尺寸接近max_size且一定下雨max_size)
+        将照片压缩到小于限制(图像尺寸接近max_size且一定小于max_size)
 
         :param max_size:照片的最大尺寸MB
         :param quality:保存质量
@@ -108,102 +171,53 @@ class Image_PIL:
             if image_size > max_size:
                 img_byte_arr.truncate(0)  # 清空数据
                 img_byte_arr.seek(0)  # 将指针重置到开头
+        return self.__image
 
+    def merge(self, other_half: str = 'self', w=True):
+        """
+        将两张照片拼接起来
 
-class IMAGE:
-
-    def Merge(self, 拼接对象='self'):
+        :param other_half:另一张照片的路径,如果是self则是自我拼接
+        :param w: 沿w方向拼接
+        """
         # 将两张同高度的照片拼接起来
-        # image_target.save(file_path, quality=100)
-        # file_path为保存的地址,需要带扩展名,如.png,.jpg,.jpeg;quality是质量,降低改值一样能起到减小照片大小的作用
-        image_path = 拼接对象
+        image_path = other_half
         if image_path == 'self':
-            width, height = self.GetSize()  # 获取长宽信息
-            image_target = Image.new('RGB', (width * 2, height))
-            image_target.paste(self.__image, box=(0, 0))
-            image_target.paste(self.__image, box=(width, 0))
-            self.__image = image_target
-            return self.__image
+            width, height = self.get_size  # 获取长宽信息
+            if w:
+                image_target = Image.new('RGB', (width * 2, height))
+                image_target.paste(self.__image, box=(0, 0))
+                image_target.paste(self.__image, box=(width, 0))
+                self.__image = image_target
+                return self.__image
+            else:
+                image_target = Image.new('RGB', (width, height * 2))
+                image_target.paste(self.__image, box=(0, 0))
+                image_target.paste(self.__image, box=(0, height))
+                self.__image = image_target
+                return self.__image
         else:
             pass
 
-    def SetWallpaperReg(self, image_path=None):
+    def save(self, target_path='', ext='', quality=100) -> bool:
         """
-        用于将照片设置为壁纸
+        将照片保存为指定格式,可以用于照片转换格式
 
-        :param image_path:照片路径,不传入时需要使用save()
-        默认为类初始化时候的照片地址
-        设置成功返回True
+        :param target_path:保存路径,不指定时默认保存为原照片所在文件夹
+        :param ext:扩展名,不指定时默认为原扩展名
+        :param quality:保存质量
         """
-        import win32gui, win32con, win32api
-        if image_path is None:
-            image_path = self.__image_path
-        # 打开指定注册表路径
-        reg_key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
-        # 最后的参数:2拉伸,0居中,6适应,10填充,0平铺
-        win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "10")
-        # 最后的参数:1表示平铺,拉伸居中等都是0
-        win32api.RegSetValueEx(reg_key, "TileWallpaper", 0, win32con.REG_SZ, "0")
-        # 刷新桌面与设置壁纸
-        win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, image_path, win32con.SPIF_SENDWININICHANGE)
-        return True
-
-    def SetWallpaperAPI(self, image_data=None):
-        """
-        从内存中的图像数据设置Windows壁纸
-
-        :param image_data:内存中的图像数据，可以是PIL Image对象或字节流
-        """
-        import ctypes, os, tempfile, io
-        if image_data is None:
-            image_data = self.__image
-        # 如果是PIL Image对象，转换为字节流
-        if isinstance(image_data, Image.Image):
-            img_byte_arr = io.BytesIO()
-            # 保存为BMP格式，Windows壁纸最好使用BMP格式
-            image_data.save(img_byte_arr, format='BMP')  # 保存为二进制数据流如同保存在物理磁盘上一样
-            img_byte_arr = img_byte_arr.getvalue()  # 获取二进制数据,用于网络传输、内存操作
+        if target_path == '':
+            target_path = file.get_file_on_dir(self.__image_path)
+        image_name = file.get_file_root(self.__image_path)
+        if ext == '':
+            ext = file.get_file_extension(self.__image_path)
+        file.ensure_exist(target_path)
+        now_time = get.now_time()
+        image_name += f'_{now_time}.{ext}'
+        target_path += f'\\{image_name}'
+        if file.check_image(target_path):
+            self.__image.save(target_path, quality=quality)
+            return True
         else:
-            img_byte_arr = image_data
-
-        # 创建一个临时文件，关闭后自动删除
-        with tempfile.NamedTemporaryFile(suffix='.bmp', delete=False) as temp_file:
-            temp_file.write(img_byte_arr)
-            temp_path = temp_file.name
-
-        try:
-            # 设置壁纸
-            SPI_SETDESKWALLPAPER = 20  # 系统操作符
-            # 调用Windows API设置壁纸
-            ctypes.windll.user32.SystemParametersInfoW(
-                SPI_SETDESKWALLPAPER,  # 操作标识符,20表示的操作为更改壁纸
-                0,  # 动态参数,更改壁纸不需要用到此参数
-                temp_path,  # 壁纸路径
-                3  # SPIF_UPDATEINIFILE | SPIF_SENDCHANGE 更新方式
-                # 如果只使用SPIF_UPDATEINIFILE(值为1)壁纸设置会保存但可能不会立即刷新;
-                # 如果只使用 SPIF_SENDCHANGE(值为2)壁纸会立即刷新但可能不会保存到系统配置中。
-            )
-        finally:
-            # 无论是否成功，都删除临时文件
-            try:
-                os.remove(temp_path)
-            except:
-                pass
-
-    def Save(self, 保存目录='', 指定格式=None):
-        # 将照片保存为指定格式,可以用于照片转换格式
-        if 保存目录 == '':
-            image_dir = os.path.dirname(self.__image_path)
-        else:
-            image_dir = 保存目录
-        image_name = os.path.basename(self.__image_path)
-        if 指定格式 == None:
-            指定格式 = os.path.splitext(image_name)[1]
-        image_name = os.path.splitext(image_name)[0] + 指定格式
-        image_path = os.path.join(image_dir, image_name)
-        if os.path.exists(image_path):
-            image_path = os.path.join(image_dir, Get.NowTime() + image_name)
-        File(image_dir).EnsureExist()
-        self.__image.save(image_path, quality=100)
-        print(f'IMAGE:图片保存成功{image_path}')
-        return image_path
+            raise TypeError(f'目标路径不是图像格式{target_path}')
