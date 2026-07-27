6c7c5d3f508ae32c95e05146d8cbdccbe0130d69
Richard <1109758969@qq.com>
2026-07-28 07:10:09 +0800

    feat(baidu_ocr):new error type

--- FrameFlow/SubAPI/ImageTools/baidu_ocr.py
+++ FrameFlow/SubAPI/ImageTools/baidu_ocr.py
@@ -32,6 +32,22 @@ from Fun.BaseTools.AsyncHTTP import AsyncJson, Task, aiohttp, AsyncHTTPManage
 logger = logging.getLogger(__name__)
 
 
+class BaiduAPIException(Exception):
+    """所有百度API相关的异常的基类"""
+    pass
+
+class NetworkError(BaiduAPIException):
+    """网络请求失败(HTTP状态码非2xx)"""
+    pass
+
+class APIError(BaiduAPIException):
+    """百度API返回错误码"""
+    def __init__(self, error_code:str,error_msg:str):
+        self.error_code = error_code
+        self.error_msg = error_msg
+        super().__init__(f"API error {error_code}: {error_msg}")
+    
+
 class AccessTokenManager:
     ACCESS_POST_URL = "https://aip.baidubce.com/oauth/2.0/token"
 
